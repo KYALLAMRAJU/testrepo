3a3db754e16b9a1040402b4551a8193b7b81b191
KYALLAMRAJU <kalyanchakravarthy1992@gmail.com>
2025-09-06 17:24:40 -0500

    testing merge conflicts

--- deletelater.py
+++ deletelater.py
@@ -1,3 +1,6 @@
 #this is a test file in testbranch 2 to test branching functionality
 adding new line from testbranch2
 can i check fastforward mergge now from testbranc2 and not changing anything in master brnach
+
+
+#updating a line in child branch to check merge confloicts and resolve it.

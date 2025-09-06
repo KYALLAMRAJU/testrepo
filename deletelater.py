b8fbc56d5f615471ba0145c85b9bfc3ed4b03a61
KYALLAMRAJU <kalyanchakravarthy1992@gmail.com>
2025-09-06 12:50:48 -0500

    added aline in delete later file from testbranch 2 to check FF merge

--- deletelater.py
+++ deletelater.py
@@ -1,2 +1,3 @@
 #this is a test file in testbranch 2 to test branching functionality
 adding new line from testbranch2
+can i check fastforward mergge now from testbranc2 and not changing anything in master brnach
